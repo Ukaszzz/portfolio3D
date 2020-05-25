6e01251e775dfcb9fedde489a2f8a51b89fba9fe
Lukasz Zola <zolalukas@gmail.com>
2020-05-25 23:37:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,553 @@
+const slideList = [
+  {
+    img: "img/misie.JPG",
+    text: "ornametal bear",
+    popup: `
+    <h1>autor: "pmoews"</h1>
+    <h2>filament: PLA  "3d active" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 220:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.2</p>
+    <p>skala: 50%</p>
+    
+    p>prędkość wydruku: 45mm/s</p>
+    <p>czas wydruku: 1h12min</p>
+    <p>wypełnienie: 25%</p>
+  
+`,
+  },
+  {
+    img: "img/tree-big.JPG",
+    text: "Dead Tree - Tabletop Scater Terrain",
+    popup: `
+    <h1>autor: "M3DM"</h1>
+    <h2>filament: PLA  "3d active" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 215:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.2</p>
+    <p>skala: 250%</p>
+    <p>prędkość wydruku: 40mm/s</p>
+    <p>czas wydruku: 6h48min</p>
+    <p>wypełnienie: 25%</p>
+  
+`,
+  },
+  {
+    img: "img/tree-small.JPG",
+    text: "Dead Tree - Tabletop Scater Terrain",
+    popup: `
+    <h1>autor: "M3DM"</h1>
+    <h2>filament: PLA  "3d active" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 215:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.12</p>
+    <p>skala: 110%</p>
+    <p>prędkość wydruku: 45mm/s</p>
+    <p>czas wydruku: 1h48min</p>
+    <p>wypełnienie: 25%</p>
+  
+`,
+  },
+  {
+    img: "img/zawijak.JPG",
+    text: "Folded Hem Attachment for Sewing Machines",
+    popup: `
+    <h1>autor: "wileur "</h1>
+    <h2>filament: PLA  "Plast-Spaw" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 207:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.2</p>
+    <p>skala: 100%</p>
+    <p>prędkość wydruku: 50mm/s</p>
+    <p>czas wydruku: 26in</p>
+    <p>wypełnienie: 20%</p>
+  
+`,
+  },
+  {
+    img: "img/car.JPG",
+    text: "solar car",
+    popup: `
+    <h1>autor: "catart3d "</h1>
+    <h2>filament: PLA  "Plast-Spaw" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 207:</p>
+    <p>temperatura stołu: 45</p>
+    <p>wysokość warstwy: 0.2</p>
+    <p>skala: 100%</p>
+    <p>prędkość wydruku: 40mm/s</p>
+    <p>czas wydruku: 1h23min</p>
+    <p>wypełnienie: 20%</p>
+  
+`,
+  },
+  {
+    img: "img/pika-anime.JPG",
+    text: "Pikachu X Kakashi (Pokemon/Naruto)",
+    popup: `
+    <h1>autor: "PatrickFanart "</h1>
+    <h2>filament: PLA  "3d active" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 215:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.2</p>
+    <p>skala: 100%</p>
+    <p>prędkość wydruku: 40mm/s</p>
+    <p>czas wydruku: 5h59min</p>
+    <p>wypełnienie: 25%</p>
+  
+`,
+  },
+  {
+    img: "img/pika-skarbonka.JPG",
+    text: "pikachu piggybank",
+    popup: `
+    <h1>autor: "jp_math "</h1>
+    <h2>filament: PLA  "3d active" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 215:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.12</p>
+    <p>skala: 25%</p>
+    <p>prędkość wydruku: 40mm/s</p>
+    <p>czas wydruku: 1h39min</p>
+    <p>wypełnienie: 20%</p>
+  
+`,
+  },
+
+  {
+    img: "img/t-rex.JPG",
+    text: "Low Poly T-Rex Skull",
+    popup: `
+  <h1>autor: "3P3D "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 210:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 25%</p>
+  <p>prędkość wydruku: 45mm/s</p>
+  <p>czas wydruku: 11h33min</p>
+  <p>wypełnienie: 20%</p>
+
+`,
+  },
+  {
+    img: "img/serce-geom.JPG",
+    text: "GEOMETRIC HEART KEY RING",
+    popup: `
+  <h1>autor: "Candice_Coreen "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 215:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 60%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 29min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+
+  {
+    img: "img/twist.JPG",
+    text: "Knurled Twist Container",
+    popup: `
+  <h1>autor: "rayperkins "</h1>
+  <h2>filament: PLA  "plast-spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 45</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 7h8min</p>
+  <p>wypełnienie: 20%</p>
+
+`,
+  },
+  {
+    img: "img/usb.JPG",
+    text: "USB SD SD Mirco Holder Combined",
+    popup: `
+  <h1>autor: " iomaa"</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 225:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 1h47min</p>
+  <p>wypełnienie: 35%</p>
+
+`,
+  },
+
+  {
+    img: "img/ksiazka.JPG",
+    text: "Book Ring",
+    popup: `
+  <h1>autor: "alohapussycat "</h1>
+  <h2>filament: PLA  "Plast-Spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 50mm/s</p>
+  <p>czas wydruku: 31min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+  {
+    img: "img/drukarka-1.JPG",
+    text: "Ender 3 Anti Snag Endcap",
+    popup: `
+  <h1>autor: "Nerys "</h1>
+  <h2>filament: PLA  "Plast-Spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 55mm/s</p>
+  <p>czas wydruku: 57min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+  {
+    img: "img/ender-rolka.JPG",
+    text: "Filament guide with roller - Ender 3",
+    popup: `
+  <h1>autor: "Tomaskom_cz "</h1>
+  <h2>filament: PLA  "Plast-Spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 3h32min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+  {
+    img: "img/ender-prowadnica.JPG",
+    text: "Ender 3 Filament Guide",
+    popup: `
+  <h1>autor: " Bouillon_CR "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 51min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+  {
+    img: "img/krancowka.JPG",
+    text: "krańcówka do ender 3",
+    popup: `
+  <h1>autor: "Łukasz Zoła "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 30mm/s</p>
+  <p>czas wydruku: 15min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/reka-troll.JPG",
+    text: "Ork Warboss",
+    popup: `
+  <h1>autor: "VidovicArts  "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 215:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 54min</p>
+  <p>wypełnienie: 30%</p>
+
+`,
+  },
+  {
+    img: "img/stabilizator.JPG",
+    text: "stabilizator do rolki filamentu",
+    popup: `
+  <h1>autor: "Łukasz Zoła "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 55mm/s</p>
+  <p>czas wydruku: 43min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/piorko.JPG",
+    text: "piórko",
+    popup: `
+  <h1>autor: "Łukasz Zoła "</h1>
+  <h2>filament: PLA  "plast-Spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 25</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 22min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/piorko2.JPG",
+    text: "Feather",
+    popup: `
+  <h1>autor: "Octomo "</h1>
+  <h2>filament: PLA  "plast-Spaw" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 207:</p>
+  <p>temperatura stołu: 25</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 50%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 26min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/klips.jpg",
+    text: "klips",
+    popup: `
+  <h1>autor: "Bouillon322 "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 14min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/pionek.JPG",
+    text: "chees",
+    popup: `
+  <h1>autor: "chuckamcknight "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 35mm/s</p>
+  <p>czas wydruku: 13min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/zakładka-serce.JPG",
+    text: "Heart Bookmark ",
+    popup: `
+  <h1>autor: "chuckamcknight "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.12</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 40mm/s</p>
+  <p>czas wydruku: 21min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/spust.JPG",
+    text: "spust ",
+    popup: `
+  <h1>autor: "jaaanik "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 225:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 45mm/s</p>
+  <p>czas wydruku: 18min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/uchwyt.JPG",
+    text: "uchwyt rączka",
+    popup: `
+  <h1>autor: " jaaanik"</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 225:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 50mm/s</p>
+  <p>czas wydruku: 43min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+  {
+    img: "img/magazynek.JPG",
+    text: "część magazynka ",
+    popup: `
+  <h1>autor: "jaaanik "</h1>
+  <h2>filament: PLA  "3d active" </h2>
+  <h3>ważniejsze parametry wydruku:</h3>
+  <p>temperatura: 220:</p>
+  <p>temperatura stołu: 50</p>
+  <p>wysokość warstwy: 0.2</p>
+  <p>skala: 100%</p>
+  <p>prędkość wydruku: 50mm/s</p>
+  <p>czas wydruku: 2h12min</p>
+  <p>wypełnienie: 100%</p>
+
+`,
+  },
+
+  {
+    img: "img/smoczki.JPG",
+    text: "cude-dragon",
+    popup: `
+    <h1>autor: "Sebastian_v650"</h1>
+    <h2>filament: PLA  "Plast-Spaw" </h2>
+    <h3>ważniejsze parametry wydruku:</h3>
+    <p>temperatura: 210:</p>
+    <p>temperatura stołu: 50</p>
+    <p>wysokość warstwy: 0.2/0.12</p>
+    <p>skala: 120/100%</p>
+    <p>prędkość wydruku: 40mm/s</p>
+    <p>czas wydruku: 3h12min</p>
+    <p>wypełnienie: 25%</p>
+  
+`,
+  },
+];
+
+const image = document.querySelector(".one img");
+const h1 = document.querySelector(".single-print__h2");
+const popup = document.querySelector(".popup");
+
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const btnProject = document.querySelector(".btn-project");
+const btnContact = document.querySelector(".btn-contact");
+
+const time = 10000;
+let active = 0;
+
+console.log(popup);
+
+const changeSlide = () => {
+  if (active === slideList.length) {
+  }
+  image.src = slideList[active].img;
+  h1.textContent = slideList[active].text;
+  popup = slideList[active].popup;
+  active++;
+  console.log(popup);
+};
+// let indexInterval = setInterval(changeSlide, time);
+
+const keyChangeSlide = (e) => {
+  if (e.keyCode === 37 || e.keyCode === 39) {
+    // clearInterval(indexInterval);
+
+    e.keyCode == 37 ? active-- : active++;
+
+    if (active == slideList.length) {
+    } else if (active < 0) {
+      active = slideList.length - 1;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    popup.innerHTML = slideList[active].popup;
+
+    console.log(slideList[active]);
+    console.log(popup);
+
+    // indexInterval = setInterval(changeSlide, time);
+  }
+};
+
+const buttonLeft = () => {
+  if (active == 0) {
+    active = slideList.length;
+  }
+  active--;
+  image.src = slideList[active].img;
+  h1.textContent = slideList[active].text;
+  popup.innerHTML = slideList[active].popup;
+
+  console.log(active);
+};
+
+const buttonRight = () => {
+  if (active === slideList.length - 1) {
+    active = 0;
+  } else {
+    active++;
+  }
+  image.src = slideList[active].img;
+  h1.textContent = slideList[active].text;
+  popup.innerHTML = slideList[active].popup;
+  console.log(active);
+};
+
+window.addEventListener("keydown", keyChangeSlide);
+btnRight.addEventListener("click", buttonRight);
+btnLeft.addEventListener("click", buttonLeft);
+btnProject.addEventListener("click", () =>
+  window.scrollTo({
+    top: document.querySelector(".prints").offsetTop,
+    behavior: "smooth",
+  })
+);
+
+btnContact.addEventListener("click", () =>
+  window.scrollTo({
+    top: document.querySelector(".contacts").offsetTop,
+    behavior: "smooth",
+  })
+);
